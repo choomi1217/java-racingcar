687d88434fb8621f8bae2865d2700ac1bb653c8b
choomi1217 <whdudal1217@naver.com>
2023-11-03 00:49:56 +0900

    test: 테스트 검증 방법 변경 및 메소드 명 변경

--- src/test/java/study/calculator/StringSeperatorTest.java
+++ src/test/java/study/calculator/StringSeperatorTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -15,27 +16,27 @@ public class StringSeperatorTest {
 
     @DisplayName(" 기본 구분자 \",\", \":\" 를 뺀 문자열을 반환")
     @Test
-    void checkDefaultDelimiter(){
+    void filteredStringByDefaultDelimiter(){
         String target = "1,2,3";
         StringSeperator stringSeperator = new StringSeperator(new Delimiter(target));
         List<String> stringList = stringSeperator.filteredString(target);
-        assertThat(stringList).doesNotContain(",");
+        assertThat(stringList).containsExactly("1","2","3");
     }
 
     @DisplayName(" 커스텀 구분자 \":\" 을 뺀 문자열을 반환" )
     @ParameterizedTest
     @MethodSource
-    void checkCustomDelimiter(String target, String expected){
+    void filteredStringByCustomDelimiter(String target, String[] expected){
         StringSeperator stringSeperator = new StringSeperator(new Delimiter(target));
         List<String> stringList = stringSeperator.filteredString(target);
-        assertThat(stringList).doesNotContain(expected);
+        assertThat(stringList).containsExactly(expected);
     }
 
-    private static Stream<Arguments> checkCustomDelimiter() {
+    private static Stream<Arguments> filteredStringByCustomDelimiter() {
         return Stream.of(
-                Arguments.of("//;\n1;2;3", ";"),
-                Arguments.of("//&\n7&8&9", "&"),
-                Arguments.of("//+\n5+36+21", "+")
+                Arguments.of("//;\n1;2;3", new String[]{"1","2","3"}),
+                Arguments.of("//&\n7&8&9", new String[]{"7","8","9"}),
+                Arguments.of("//+\n5+36+21", new String[]{"5","36","21"})
         );
     }
 
